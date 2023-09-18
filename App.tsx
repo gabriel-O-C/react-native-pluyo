8140671a511a2c7b5479b60d67aed3daeb4e3f14
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-09-18 15:58:06 -0300

    refactor: use listQuestions

--- App.tsx
+++ App.tsx
@@ -1,44 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { db } from "./firebaseConfig";
+import questionService from "./src/services/questionService/questionService";
+import { Question } from "./src/types/question";
 
-interface Question {
-  answer: string;
-  title: string;
-  alternatives: string[];
-  question: string;
-}
+
 
 export default function App() {
   const [questions, setQuestions] = useState<Question[]>([]);
 
   useEffect(() => {
     async function fetchData() {
-      const querySnapshot = await getDocs(collection(db, "questions"));
-      querySnapshot.forEach((doc) => {
-        let data = doc.data();
-        const arr = [];
-        let question: Question = data.questions
-          .flatMap((item: Question) => item)
-          .flat(2);
-        arr.push(question);
+      const data = await questionService.listQuestions()
+      setQuestions(data)
+      };
 
-        setQuestions(arr.flat(2));
-      });
-    }
     fetchData();
   }, []);
-
+console.log(questions)
   return (
     <View style={styles.container}>
       {questions.length > 0 ? (
         <View>
           <Text>Fill in the missing word.</Text>
           {questions.map((question) => (
+            
             <Text key={question.title}>
-              {question.title}
+              {question.question}
             </Text>
           ))}
           <TouchableOpacity>
---
+++ src/types/question.ts
@@ -0,0 +1,6 @@
+export interface Question {
+  answer: string;
+  title: string;
+  alternatives: string[];
+  question: string;
+}
