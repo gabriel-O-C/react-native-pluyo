13e9ffb8bcd87fb194f1234e16c7aa2516c5674f
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-09-18 13:02:59 -0300

    💄: add default colors

---
+++ src/assets/styles/theme/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    primary: {
+      main: "#3c6c82",
+      light: "#26e5eb"
+    },
+    secondary: {
+      main: "#6392A5"
+    },
+    danger: {
+      main: "#ff7e89"
+    }
+
+  }
+}
